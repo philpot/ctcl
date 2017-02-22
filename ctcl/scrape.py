805a1453858b3d9c5c9684e366db579ffd02e5c6
Andrew Philpot <andrew.philpot@gmail.com>
2017-02-22 06:55:44 -0800

    all fields

--- ctcl/scrape.py
+++ ctcl/scrape.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function
 
+import sys
 import logging
+import argparse
 
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=logging.DEBUG,
@@ -34,23 +36,44 @@ class College(object):
 
     
 
-def scrape(pathname=default_pathname):
+DEFAULT_PARSER = "lxml"
+
+def scrape(pathname=default_pathname, parser=DEFAULT_PARSER):
     c = College()
+    whitespace = u'\u22c5 \n\t'
     c.pathname = pathname
     with open(pathname, 'r') as f:
-        soup = BeautifulSoup(f, 'html.parser')
+        soup = BeautifulSoup(f, parser)
         c.soup = soup
-    return c
-    tab_items = [x for x in soup.find_all("div", class_="mks_tab_item")]
-    (contact, enrollment, tuition, quick) = tab_items
-    contact_children = [x for x in contact.children]
-    logging.info("CC {}".format(contact_children))
-    c.name = contact_children[1].strip()
-    c.street = contact_children[3].strip()
-    c.city_state_zip = contact_children[5].strip()
-    c.phone = contact_children[7].strip()
-    enrollment_children = [x for x in enrollment.children]
-    c.enrollment = enrollment_children
+    c.tab_items = [x for x in soup.find_all("div", class_="mks_tab_item")]
+    (contact, enrollment, tuition, quick) = c.tab_items
+    contact_lines = list(contact.children)
+    c.name = contact_lines[1].strip()
+    c.street = contact_lines[3].strip()
+    c.city_state_zip = contact_lines[5].strip()
+    c.phone = contact_lines[7].strip()
+
+    enroll_lines = list(enrollment.children)
+    c.size = enroll_lines[1].strip(whitespace)
+    c.ratio = enroll_lines[3].strip(whitespace)
+
+    tuition_lines = list(tuition)
+    c.tuition = tuition_lines[1].strip(whitespace)
+    c.aid = tuition_lines[3].strip(whitespace)
+
+    quick_lines = list(quick)
+    c.aid_link = quick_lines[3]['href']
     return c
 
-    
+
+# call main() if this is run as standalone
+
+def main(parser):
+    return scrape(parser=parser)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--parser", type=str, default=DEFAULT_PARSER)
+    args = parser.parse_args()
+    c = main(args.parser)
+
