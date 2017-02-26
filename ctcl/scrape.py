868f686d6cd4915c01263713179d06ab6313f2fe
Andrew Philpot <andrew.philpot@gmail.com>
2017-02-26 14:07:42 -0800

    generic ascii, fetch all tuition

--- ctcl/scrape.py
+++ ctcl/scrape.py
@@ -3,8 +3,10 @@
 from __future__ import print_function
 
 import sys
+import os
 import logging
 import argparse
+import csv
 
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=logging.DEBUG,
@@ -26,7 +28,7 @@ logging.getLogger('').addHandler(console)
 logging.info('Jackdaws love my big sphinx of quartz.')
 from bs4 import BeautifulSoup
 
-default_pathname = "/Users/philpot/ws/ctcl/data/agnes-scott-college"
+DEFAULT_PATHNAME = "/Users/philpot/ws/ctcl/data/agnes-scott-college"
 
 # contact is <div class="mks_tab_item"><div class="nav">Contact</div>
 
@@ -34,11 +36,25 @@ class College(object):
     def __init__(self, *args, **kwargs):
         pass
 
-    
+DEFAULT_OUTPUT_DIRECTORY = "/tmp/"
+
+def make_output_pathname(input_pathname, output_directory=DEFAULT_OUTPUT_DIRECTORY):
+    return os.path.join(output_directory, os.path.basename(input_pathname) + ".csv")
 
 DEFAULT_PARSER = "lxml"
 
-def scrape(pathname=default_pathname, parser=DEFAULT_PARSER):
+def ascii(thing, discard=u'\u22c5 \n\t'):
+    try:
+        thing = thing.string
+    except:
+        thing = str(thing)
+    thing = thing.encode('ascii', 'ignore')
+    if discard:
+        thing = thing.strip(discard)
+    return thing
+
+
+def scrape(pathname=DEFAULT_PATHNAME, parser=DEFAULT_PARSER):
     c = College()
     whitespace = u'\u22c5 \n\t'
     c.pathname = pathname
@@ -58,11 +74,21 @@ def scrape(pathname=default_pathname, parser=DEFAULT_PARSER):
     c.ratio = enroll_lines[3].strip(whitespace)
 
     tuition_lines = list(tuition)
-    c.tuition = tuition_lines[1].strip(whitespace)
-    c.aid = tuition_lines[3].strip(whitespace)
+    # c.tuition = tuition_lines[1].strip(whitespace)
+    # c.aid = tuition_lines[3].strip(whitespace + ":")
+    return c
+    c.finances = " ".join([ascii(t) for t in tuition_lines[2:]])
 
     quick_lines = list(quick)
     c.aid_link = quick_lines[3]['href']
+
+    output_file = make_output_pathname(pathname)
+    with open(output_file, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        # row = [c.name, c.street, c.city_state_zip, c.phone, c.size, c.ratio, c.tuition, c.aid, c.aid_link]
+        row = [c.name, c.street, c.city_state_zip, c.phone, c.size, c.ratio, c.finances, c.aid_link]
+        row = [convert(val) for val in row]
+        writer.writerow(row)
     return c
 
 
